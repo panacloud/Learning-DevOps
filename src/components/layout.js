31aabcd6ec4707d3e6eeecc56c9897ea5d121e1e
MuhammadMohsin <mmohsinkhalid@outlook.com>
2020-04-29 17:22:03 +0500

    Add nav bar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,13 +24,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <Navbar />
         <main>{children}</main>
       </div>
