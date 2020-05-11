b8e22c93965549645e785cc1dcdcb118cbb2cb4a
Daniyal Nagori <root@Daniyals-Mac-mini.local>
2020-05-11 18:55:28 +0500

    carousel slider with dynamic data from contentful

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, withPrefix } from "gatsby";
+import { Link, withPrefix, graphql, StaticQuery } from "gatsby";
 import MobileStepper from "@material-ui/core/MobileStepper";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -24,6 +24,25 @@ class Carousel extends React.Component {
     activeStep: 0,
   };
 
+  items = [
+    {
+      node: {
+        id: "31d6a4bf-62e5-5211-9d87-2537f9628a52",
+        excerpt:
+          "A one year AI program in Data Science, Machine Learning, and Deep Learning.",
+        frontmatter: {
+          title: "Artificial Intelligence (AI)",
+          date: "Unknown",
+          path: "/",
+          image: {
+            publicURL:
+              "//images.ctfassets.net/6y7x6a0he6ux/fnmiegLZP7y4t5vqUvrsl/bc8175659334f378aeeed53521a18d20/ai.jpeg",
+          },
+        },
+      },
+    },
+  ];
+
   handleNext = () => {
     this.setState((prevState) => ({
       activeStep: prevState.activeStep + 1,
@@ -44,46 +63,113 @@ class Carousel extends React.Component {
     const { activeStep } = this.state;
     const { classes, items } = this.props;
     const maxSteps = items.length;
+
+    console.log("items: ", items);
+    const filteredData = [];
     return (
       <Paper elevation={0}>
+        <StaticQuery
+          query={graphql`
+            {
+              allContentfulFranchisee {
+                nodes {
+                  courseCatalog {
+                    programsAvailable {
+                      title
+                      id
+                      shortDescription {
+                        shortDescription
+                      }
+                      image {
+                        title
+                        file {
+                          url
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={(data) => {
+            const defaultData =
+              data.allContentfulFranchisee.nodes[0].courseCatalog
+                .programsAvailable;
+            console.log("defaultData: ", defaultData);
+            defaultData.forEach((dataItem, dataIndex) => {
+              const filteredItem = Object.assign(
+                {},
+                {
+                  node: {
+                    id: dataItem.id,
+                    excerpt: dataItem.shortDescription.shortDescription,
+                    frontmatter: {
+                      title: dataItem.title,
+                      date: "Unknown",
+                      path: "/programs",
+                      image: {
+                        publicURL: dataItem.image.file.url,
+                      },
+                    },
+                  },
+                }
+              );
+              filteredData.push(filteredItem);
+            });
+            this.items = filteredData;
+          }}
+        ></StaticQuery>
+
         <div>
-          <Typography>{items[activeStep].title}</Typography>
+          <Typography>
+            {(this.items[activeStep] || { title: "qwerty" }).title}
+          </Typography>
           <AutoPlaySwipeableViews
             axis={"x"}
             enableMouseEvents
             index={activeStep}
             onChangeIndex={this.handleStepChange}
           >
-            {items.map((item, index) => {
-              const {
-                node: {
-                  excerpt,
-                  frontmatter: {
-                    path,
-                    title,
-                    image: { publicURL },
-                  },
-                },
-              } = item;
-              return (
-                <div key={index}>
-                  {Math.abs(activeStep - index) <= 2 ? (
-                    <Card>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image={withPrefix(publicURL)}
-                      />
-                      <CardContent>
-                        <Typography component="h2" gutterBottom variant="h5">
-                          <Link to={path}>{title}</Link>
-                        </Typography>
-                        <Typography component="p">{excerpt}</Typography>
-                      </CardContent>
-                    </Card>
-                  ) : null}
-                </div>
-              );
-            })}
+            {/* {JSON.stringify(this.items)} */}
+            {this.items.length > 0
+              ? this.items.map((item, index) => {
+                  console.log("this.items > item: ", item);
+                  console.log("this.items > index: ", index);
+                  const {
+                    node: {
+                      excerpt,
+                      frontmatter: {
+                        path,
+                        title,
+                        image: { publicURL },
+                      },
+                    },
+                  } = item;
+                  return (
+                    <div key={index}>
+                      {Math.abs(activeStep - index) <= 2 ? (
+                        <Card>
+                          <CardMedia
+                            className={classes.cardMedia}
+                            image={publicURL}
+                          />
+                          <CardContent>
+                            <Typography
+                              component="h2"
+                              gutterBottom
+                              variant="h5"
+                            >
+                              <Link to={path}>{title}</Link>
+                            </Typography>
+                            <Typography component="p">{excerpt}</Typography>
+                          </CardContent>
+                        </Card>
+                      ) : null}
+                    </div>
+                  );
+                })
+              : null}
           </AutoPlaySwipeableViews>
           <MobileStepper
             activeStep={activeStep}
@@ -106,7 +192,7 @@ class Carousel extends React.Component {
               </Button>
             }
             position="static"
-            steps={maxSteps}
+            steps={this.items.length}
           />
         </div>
       </Paper>
