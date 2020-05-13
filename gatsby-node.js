dffc7fc5ec6b0887deea7c115f577f8c30864210
Zia Khan <ziaukhan@hotmail.com>
2020-05-13 15:37:13 +0500

    backing up previous page creation code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,5 @@
 const path = require(`path`);
 
-// Implement the Gatsby API “onCreatePage”. This is
-// called after every page is created.
-/*exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions;
-
-  // Only update the `/details` page.
-  if (page.path.match(/^\/details/)) {
-    // page.matchPath is a special key that's used for matching pages
-    // with corresponding routes only on the client.
-    page.matchPath = "/details/*";
-
-    // Update the page.
-    createPage(page);
-  }
-};*/
-
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
@@ -73,6 +57,23 @@ https://www.gatsbyjs.org/tutorial/part-seven/
 https://stackoverflow.com/questions/55756994/how-to-create-dynamic-route-in-gatsby
 Here we are using method 1
 */
+
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+/*exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  // Only update the `/details` page.
+  if (page.path.match(/^\/details/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/details/*";
+
+    // Update the page.
+    createPage(page);
+  }
+};*/
+
 
 
 
