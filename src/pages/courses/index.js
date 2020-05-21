39a3d763a5b475b1c62d19329488d826ba091a43
Zia Khan <ziaukhan@hotmail.com>
2020-05-21 14:40:34 +0500

    updated courses page

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -5,13 +5,42 @@ import SEO from "../../components/SEO";
 import Page from "../../components/Page";
 //import List from "../../components/List";
 import ListOfPrograms from "../../components/ListOfPrograms";
+import useCoursesData from "../../hooks/useCoursesData";
+import { Link } from "gatsby";
 
 const Programs = (props) => {
-  //const programs = props.data.allMarkdownRemark.edges;
+  const programsAvailable = useCoursesData();
+  let courses = [];
+
+  programsAvailable.map((p, index) => {
+    courses = courses.concat(p.technicalTrack.courses);
+    if(index == programsAvailable.length - 1){
+        courses = courses.concat(p.appDevelopmentTrack.courses);
+        courses = courses.concat(p.innovationTrack.courses);
+    }
+  });
+  
+  //console.log("Programs: " + JSON.stringify(programsAvailable));
   return (
     <Page title="Course Catalog">
       <SEO title="Course Catalog" />
-      {/*<List items={programs} />*/}
+      {courses.map((c, index) => {
+        return (
+            <div>
+                <Link to={ "/programs/tracks/courses/" + c.courseNumber}>
+                        {c.courseNumber + " " + c.title}
+                </Link>
+              
+                <div
+                    dangerouslySetInnerHTML={{
+                    __html: c.description.childMarkdownRemark.html,
+                    }}
+                ></div>
+                <br/>
+                <br/>
+            </div>
+        )
+      })}
       
     </Page>
   );
